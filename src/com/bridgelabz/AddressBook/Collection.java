8f40d5d20528d9666dbb64097f68c504d2e3dcee
Ameet Jadhav <ammugs11@gmail.com>
2022-09-29 18:25:44 +0530

    added to add multiple person to address book

---
+++ src/com/bridgelabz/AddressBook/Collection.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.AddressBook;
+
+import java.util.Collections;
+
+public class Collection extends Collections {
+
+	public void readData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void DisplayContacts() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void editData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
